c174d83875f84c1ffa0b819a762b1da1956f8071
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-02-06 15:07:45 -0600

    Update 1

--- a3q1v1_testdriver.py
+++ a3q1v1_testdriver.py
@@ -5,6 +5,7 @@
 # Instructor: Gang Li
 
 from testing_blackbox_recentTrend import recentTrend
+
 # i.e. the recentTrend() function is sitting in a file called "testing_blackbox_recentTrend.py"
 
 # The empty list is a special case, so always test it!
@@ -14,46 +15,44 @@ result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#A list of fewer than 20 elements.
+# A list of fewer than 20 elements.
 test = [0, 1, 2]
 expected = 2
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#Testing a list of 2 elements, a One and a Zero.
+# Testing a list of 2 elements, a One and a Zero.
 test = [1, 0]
 expected = 1
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#Testing a list of 20 elements, where each is a unique value.
+# Testing a list of 20 elements, where each is a unique value.
 test = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 expected = 19
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#Testing a list of 20 elements, with the smaller values before the bigger values.
+# Testing a list of 20 elements, with the smaller values before the bigger values.
 test = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 expected = 1
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#Testing a list of 20 elements, with the bigger values before the smaller values.
+# Testing a list of 20 elements, with the bigger values before the smaller values.
 test = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 expected = 1
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
 
-#Testing a list of a mixture of elements.
+# Testing a list of a mixture of elements.
 test = [0, 1, 2, 0, 1, 2, 2, 1, 0, 1, 0, 2, 0, 2, 1, 1, 0, 1, 0]
 expected = 1
 result = recentTrend(test)
 if result != expected:
     print("Testing recentTrend() with", test, "   Expected:", expected, " Got: ", result)
-
-
--- a3q2v1_testdriver.py
+++ a3q2v1_testdriver.py
@@ -6,7 +6,7 @@
 
 from a3q2v1 import closest_to_zero
 
-#Testing one zero value
+# Testing one zero value
 test1 = 1
 test2 = 0
 test3 = 3
@@ -15,7 +15,7 @@ result = closest_to_zero(test1, test2, test3)
 if result != expected:
     print("Testing recentTrend() with", test1, test2, test3, "   Expected:", expected, " Got: ", result)
 
-#Testing three positive values
+# Testing three positive values
 test1 = 1
 test2 = 2
 test3 = 3
@@ -24,7 +24,7 @@ result = closest_to_zero(test1, test2, test3)
 if result != expected:
     print("Testing recentTrend() with", test1, test2, test3, "   Expected:", expected, " Got: ", result)
 
-#Testing three negative values
+# Testing three negative values
 test1 = -2
 test2 = -1
 test3 = -3
@@ -33,7 +33,7 @@ result = closest_to_zero(test1, test2, test3)
 if result != expected:
     print("Testing recentTrend() with", test1, test2, test3, "   Expected:", expected, " Got: ", result)
 
-#Testing a mixture of positive and negative values
+# Testing a mixture of positive and negative values
 test1 = -1
 test2 = 2
 test3 = 3
@@ -42,7 +42,7 @@ result = closest_to_zero(test1, test2, test3)
 if result != expected:
     print("Testing recentTrend() with", test1, test2, test3, "   Expected:", expected, " Got: ", result)
 
-#Testing a draw between a positive and negative value
+# Testing a draw between a positive and negative value
 test1 = 1
 test2 = -1
 test3 = 3
@@ -51,7 +51,7 @@ result = closest_to_zero(test1, test2, test3)
 if result != expected:
     print("Testing recentTrend() with", test1, test2, test3, "   Expected:", expected, " Got: ", result)
 
-#Testing two values in a draw AND a zero
+# Testing two values in a draw AND a zero
 test1 = 0
 test2 = -1
 test3 = 1
