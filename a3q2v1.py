7a8ead8c5eea62a6530ad3aec2ed9f41f973feda
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-02-06 15:42:35 -0600

    Update 1

--- a3q2v1.py
+++ a3q2v1.py
@@ -5,6 +5,16 @@
 # Instructor: Gang Li
 
 def closest_to_zero(num1, num2, num3):
+    """
+    Purpose:
+        Calculates the smallest number based on the possible scenarios, and the numbers chosen.
+    Parameters:
+        num1: The first integer number chosen.
+        num2: The second integer number chosen.
+        num3: The third integer number chosen.
+    Return:
+        Returns which of the three numbers was the smallest (num1 for Number 1, num2 for Number 2, and num3 for Number3)
+    """
     lowest = min(abs(num1), abs(num2), abs(num3))
 
     if lowest == 0:
